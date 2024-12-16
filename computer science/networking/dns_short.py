1313e884b6aa16eb069f8387387337baa128268c
Vincent Archambault-Bouffard <vincentarchambault@icloud.com>
2024-12-15 22:49:17 -0500

    Getting better, with colors !

--- computer science/networking/dns_short.py
+++ computer science/networking/dns_short.py
@@ -3,7 +3,7 @@ import manim as m
 # Global script parameters
 computer_scale = 0.25
 old_dns_opacity = 0.4
-tex_default_size = 36
+tex_default_size = 32
 myLaTeX = m.TexTemplate()
 myLaTeX.add_to_preamble(r"\usepackage[T1]{fontenc} \usepackage{fontawesome}")
 
@@ -73,32 +73,38 @@ class DNS(m.Scene):
         recursive, recursive_ip, recursive_name = self.recursive_resolver(user=user)
 
         # Display dns server
-        self.add_root_dns(recursive=recursive,
+        root, root_name = self.add_root_dns(
+                          recursive=recursive,
                           request=".ca ?",
                           response="185.159.196.2",
                           dns_address=r"{\faGlobe} 192.203.230.10")
-        self.add_tld_dns(recursive=recursive,
+        tld, tld_name = self.add_tld_dns(
+                         recursive=recursive,
                          request="cmaisonneuve.qc.ca ?",
                          response="162.219.54.2",
-                         dns_address=r"{\faGlobe} 185.159.196.2"),
-        self.add_authoritative_dns(recursive=recursive,
+                         dns_address=r"{\faGlobe} 185.159.196.2")
+        auth, auth_name = self.add_authoritative_dns(
+                                   recursive=recursive,
                                    request="www.cmaisonneuve.qc.ca ?",
                                    response="151.101.2.133",
                                    dns_address=r"{\faGlobe} 162.219.54.2")
 
         # Transmit the IP address to the user
         arrow = m.Arrow(start=recursive.get_corner(m.LEFT),
-                        end=user.get_corner(m.UP))
+                        end=user.get_corner(m.RIGHT))
+        arrow.color = m.YELLOW
         ip_address = myTex("151.101.2.133")
-        ip_address.next_to(arrow, m.RIGHT)
+        ip_address.set_color(m.YELLOW)
+        ip_address.rotate(arrow.get_angle() + m.PI)
+        ip_address.next_to(arrow, m.ORIGIN)
+        ip_address.shift(m.UP * 0.4 + m.LEFT * 0.4)
         self.play(m.GrowArrow(arrow))
         self.play(m.FadeIn(ip_address))
         self.wait(2)
 
-        self.play(m.FadeOut(arrow, ip_address),
+        self.play(m.FadeOut(arrow, ip_address, recursive_ip),
                   recursive.animate.set_opacity(old_dns_opacity),
-                  recursive_name.animate.set_opacity(old_dns_opacity),
-                  recursive_ip.animate.set_opacity(old_dns_opacity))
+                  recursive_name.animate.set_opacity(old_dns_opacity))
 
         # Change ??? to the IP address
         maisonneuve_new_ip = myTex(r"{\faUniversity} ",
@@ -106,17 +112,32 @@ class DNS(m.Scene):
         maisonneuve_new_ip.move_to(maisonneuve_ip.get_center())
         arrow2 = m.Arrow(start=user.get_corner(m.RIGHT),
                          end=maisonneuve.get_corner(m.LEFT))
+        arrow2.color = m.BLUE
         self.play(m.Transform(maisonneuve_ip, maisonneuve_new_ip))
         self.wait(2)
         self.play(m.GrowArrow(arrow2))
+        self.wait(2)
+
+        # DNS is a pyramidal system
+        for x, y in [(root, root_name), (tld, tld_name), (auth, auth_name),
+                     (recursive, recursive_name)]:
+            x.set_opacity(1)
+            y.set_opacity(1)
+            self.play(m.Indicate(x), m.Indicate(y))
+            self.wait(2)
+            x.set_opacity(old_dns_opacity)
+            y.set_opacity(old_dns_opacity)
+        self.wait(2)
 
     def create_user_and_maisonneuve(self):
         user = computer().scale(computer_scale)
+        user.color = m.BLUE
         user.shift(m.LEFT * 4 + m.DOWN * 2)
         user_ip = myTex(r"{\faUser} 192.168.1.15")
         user_ip.next_to(user, m.DOWN)
 
         maisonneuve = computer().scale(computer_scale)
+        maisonneuve.color = m.DARK_BLUE
         maisonneuve.shift(m.RIGHT * 4 + m.DOWN * 2)
         maisonneuve_ip = myTex(r"{\faUniversity} ", "???.???.???.???")
         maisonneuve_ip.set_color_by_tex("???", m.RED)
@@ -125,20 +146,23 @@ class DNS(m.Scene):
 
     def recursive_resolver(self, *, user: m.VGroup):
         recursive = computer().scale(computer_scale)
+        recursive.color = m.GREEN
         recursive.shift(m.DOWN * 0.5)
 
         recursive_name = myTex("serveur récursif")
         recursive_name.next_to(recursive, m.DOWN)
 
-        recursive_ip = myTex(r"{\faServer} ", "1.1.1.1")
+        recursive_ip = myTex(r"{\faGlobe} ", "1.1.1.1")
         recursive_ip.next_to(recursive_name, m.DOWN)
 
         arrow2 = m.Arrow(start=user.get_corner(m.UP), end=recursive.get_corner(m.UL))
+        arrow2.color = m.YELLOW
         address = ["www", ".",
                    "cmaisonneuve", ".",
                    "qc", ".",
                    "ca"]
         request2 = myTex(*address, " ?")
+        request2.color = m.YELLOW
         request2.rotate(arrow2.get_angle())
         request2.next_to(arrow2, m.ORIGIN)
         request2.shift(m.UP * 0.4 + m.LEFT * 0.4)
@@ -171,6 +195,7 @@ class DNS(m.Scene):
         Add the root DNS request to the scene.
         """
         dns = computer().scale(computer_scale)
+        dns.color = m.GREEN_D
         dns.shift(m.LEFT * 4 + m.UP * 1.5)
         dns_name = myTex("serveur racine")
         dns_name.next_to(dns, m.DOWN)
@@ -180,8 +205,9 @@ class DNS(m.Scene):
 
         arrow_in = m.Arrow(start=recursive.get_corner(m.UL),
                            end=dns_group.get_corner(m.UR))
-
+        arrow_in.color = m.YELLOW
         request_tex = myTex(request)
+        request_tex.color = m.YELLOW
         request_tex.rotate(arrow_in.get_angle() + m.PI)
         request_tex.next_to(arrow_in, m.ORIGIN)
         request_tex.shift(m.UP * 0.2 + m.RIGHT * 0.2)
@@ -189,13 +215,17 @@ class DNS(m.Scene):
         arrow_out = m.Arrow(start=dns_group.get_corner(m.UR),
                             end=recursive.get_corner(m.UL))
         arrow_out.shift(m.DOWN * 0.5 + m.LEFT * 0.3)
+        arrow_out.color = m.YELLOW
 
         response_tex = myTex(response)
+        response_tex.color = m.YELLOW
         response_tex.rotate(arrow_out.get_angle())
         response_tex.next_to(arrow_out, m.ORIGIN)
         response_tex.shift(m.DOWN * 0.25 + m.LEFT * 0.25)
 
         self.add_dns(dns, dns_name, dns_ip, arrow_in, request_tex, arrow_out, response_tex)
+
+        return (dns, dns_name)
 
     def add_tld_dns(self,
                     *,
@@ -207,6 +237,7 @@ class DNS(m.Scene):
         Add the top level domain DNS request to the scene.
         """
         dns = computer().scale(computer_scale)
+        dns.color = m.GREEN_D
         dns.shift(m.UP * 3)
         dns_name = myTex("serveur du domaine de premier niveau")
         dns_name.next_to(dns, m.DOWN)
@@ -216,18 +247,22 @@ class DNS(m.Scene):
         arrow_in = m.Arrow(start=recursive.get_corner(m.UP),
                            end=dns_ip.get_corner(m.DOWN))
         arrow_in.shift(m.RIGHT * 0.25)
-
+        arrow_in.color = m.YELLOW
         request_tex = myTex(request)
+        request_tex.color = m.YELLOW
         request_tex.next_to(arrow_in, m.RIGHT)
 
         arrow_out = m.Arrow(start=dns_ip.get_corner(m.DOWN),
                             end=recursive.get_corner(m.UP))
         arrow_out.shift(m.LEFT * 0.25)
-
+        arrow_out.color = m.YELLOW
         response_tex = myTex(response)
+        response_tex.color = m.YELLOW
         response_tex.next_to(arrow_out, m.LEFT)
 
         self.add_dns(dns, dns_name, dns_ip, arrow_in, request_tex, arrow_out, response_tex)
+
+        return (dns, dns_name)
 
     def add_authoritative_dns(self,
                               *,
@@ -239,31 +274,38 @@ class DNS(m.Scene):
         Add the authoritative DNS request to the scene.
         """
         dns = computer().scale(computer_scale)
+        dns.color = m.GREEN_D
         dns.shift(m.RIGHT * 4 + m.UP * 1.5)
         dns_name = myTex("serveur faisant autorité")
         dns_name.next_to(dns, m.DOWN)
         dns_ip = myTex(dns_address)
         dns_ip.next_to(dns_name, m.DOWN)
-        dns_group = m.VGroup(dns, dns_name, dns_ip)
 
         arrow_in = m.Arrow(start=recursive.get_corner(m.UR),
                            end=dns.get_corner(m.UL))
+        arrow_in.scale(0.9)
+        arrow_in.color = m.YELLOW
 
-        request_tex = myTex(request)
-        request_tex.rotate(arrow_in.get_angle())
-        request_tex.next_to(arrow_in, m.ORIGIN)
-        request_tex.shift(m.UP * 0.2 + m.LEFT * 0.2)
+        request_tex = m.Tex(request, tex_template=myLaTeX, font_size=30)
+        request_tex.color = m.YELLOW
+        request_tex.next_to(arrow_in, m.LEFT)
+        request_tex.shift(m.RIGHT * 1)
 
-        arrow_out = m.Arrow(start=dns_group.get_corner(m.UL),
+        arrow_out = m.Arrow(start=dns.get_corner(m.UL),
                             end=recursive.get_corner(m.UR))
+        arrow_out.color = m.YELLOW
+        arrow_out.scale(0.75)
         arrow_out.shift(m.DOWN * 2 + m.RIGHT * 0.75)
 
         response_tex = myTex(response)
+        response_tex.color = m.YELLOW
         response_tex.rotate(arrow_out.get_angle() + m.PI)
         response_tex.next_to(arrow_out, m.ORIGIN)
         response_tex.shift(m.DOWN * 0.25 + m.RIGHT * 0.25)
 
         self.add_dns(dns, dns_name, dns_ip, arrow_in, request_tex, arrow_out, response_tex)
+
+        return (dns, dns_name)
 
     def add_dns(self, dns, dns_name, dns_ip, arrow_in, request_tex, arrow_out, response_tex):
         self.play(m.FadeIn(dns, dns_name, dns_ip), run_time=2)
